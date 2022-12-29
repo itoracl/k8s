34500f9fe575226487079ddaf5d088528c2d879d
cloudshell <itoracle@icloud.com>
2022-12-29 19:27:52 +0000

    Update 'debug.groovy'

--- debug.groovy
+++ debug.groovy
@@ -6,9 +6,9 @@ pipeline {
     stages { 
         stage('Deploy') { 
             steps { 
-            //    sh 'ansible-playbook debug.yaml'//
+                sh 'ansible-playbook debug.yaml'//
             //   echo $ANS_HOME
-               ansiblePlaybook becomeUser: 'cloudshell', colorized: true, forks: 2, playbook: 'debug.yaml'
+            //   ansiblePlaybook becomeUser: 'cloudshell', colorized: true, forks: 2, playbook: 'debug.yaml'
             }
         }
     }
