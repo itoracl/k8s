7b7f5c2437e4643e8d51f1c679e1b56d1e06375b
cloudshell <itoracle@icloud.com>
2023-01-06 20:34:56 +0000

    Update 'k8s-init.groovy'

--- k8s-init.groovy
+++ k8s-init.groovy
@@ -6,9 +6,9 @@ pipeline {
     stages { 
         stage('Deploy') { 
             steps { 
-            //    sh 'ansible-playbook debug.yaml'//
+                sh 'ansible-playbook init-phb.yaml'//
             //   echo $ANS_HOME
-               ansiblePlaybook becomeUser: 'cloudshell', colorized: true, forks: 2, playbook: 'debug.yaml'
+            //   ansiblePlaybook becomeUser: 'cloudshell', colorized: true, forks: 2, playbook: 'debug.yaml'
             }
         }
     }
