6bd4354afda32b423fb5385b4a198186cf989cba
cloudshell <itoracle@icloud.com>
2023-01-09 13:37:59 +0000

    Update 'k8s-init.groovy'

--- k8s-init.groovy
+++ k8s-init.groovy
@@ -3,22 +3,26 @@ pipeline {
     stages { 
         stage('Deploy Kubernetes Cluster Conform to kubeadm 1.26 kubernetes.io official') { 
            parallel {
-                stage('Apply system requirements')
+             stage('Apply system requirements'){
                    steps { 
                      sh 'ansible-playbook init-phd.yaml'
                    }
-                stage('Installing containerd container runtime')
+             }
+             stage('Installing containerd container runtime'){
                    steps{
                      sh 'ansible-playbook init-phb.yaml'
                    }
-                stage('Configuring systemd cgroup driver')
+             }
+             stage('Configuring systemd cgroup driver'){
                    steps{
                      sh 'ansible-playbook init-phc.yaml'
                    }
+             }
                 stage('Installing kubeadm, kubelet and kubectl'){
                    steps { 
                      sh 'ansible-playbook init-pha.yaml'
                    }
+                }
            }
           stage('Bootstrap cluster with kubeadm'){
             steps { 
