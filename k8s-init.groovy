ba3e4e6e83cd95e46c49d7c1c45cbcf8db916b47
cloudshell <itoracle@icloud.com>
2023-01-10 17:44:20 +0000

    Update 'k8s-init.groovy'

--- k8s-init.groovy
+++ k8s-init.groovy
@@ -4,7 +4,7 @@ pipeline {
         parallelsAlwaysFailFast()
     }
     stages { 
-        stage('Deploy Kubernetes \nCluster Conform to kubeadm 1.26 kubernetes.io official'){ 
+        stage('Deploy Kubernetes Cluster conforms to kubeadm 1.26 kubernetes.io official'){ 
            parallel {
              stage('Apply system requirements'){
                    steps { 
@@ -13,41 +13,41 @@ pipeline {
              }
              stage('Installing containerd container runtime'){
                    steps{
-                     sh 'ansible-playbook init-phb.yaml'
+                     sh 'ansible-playbook -i files/hosts init-phb.yaml'
                    }
              }
              stage('Configuring systemd cgroup driver'){
                    steps{
-                     sh 'ansible-playbook init-phc.yaml'
+                     sh 'ansible-playbook -i files/hosts init-phc.yaml'
                    }
              }
              stage('Installing kubeadm, kubelet and kubectl'){
                    steps { 
-                     sh 'ansible-playbook init-pha.yaml'
+                     sh 'ansible-playbook -i files/hosts init-pha.yaml'
                    }
              }
            }
         }
          stage('Bootstrap cluster with kubeadm'){
             steps { 
-                sh 'ansible-playbook init.yaml'
+                sh 'ansible-playbook -i files/hosts init.yaml'
             }
          }
         stage('Install necessary kubernetes cluster services'){ 
            parallel {
              stage('Installing flannel network overlay '){
                    steps { 
-                     sh 'ansible-playbook init-ppha.yaml'
+                     sh 'ansible-playbook -i files/hosts init-ppha.yaml'
                    }
              }
              stage('Installing kubernetes dashboard recommended'){
                    steps{
-                     sh 'ansible-playbook init-pphb.yaml'
+                     sh 'ansible-playbook -i files/hosts init-pphb.yaml'
                    }
              }
              stage('Installing ingress controller'){
                    steps{
-                     sh 'ansible-playbook init-pphc.yaml'
+                     sh 'ansible-playbook -i files/hosts init-pphc.yaml'
                    }
              }
            }
